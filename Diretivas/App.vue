29424206f0b73545df294cb388e79e29acb412f4
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-24 12:34:25 -0300

    Add, diretivas

---
+++ Diretivas/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Minha lista de tarefas!</h1>
+
+    <ul>
+      <li 
+        v-for="task in tasks"
+        :key="`${task}-${index}`"
+      >
+        {{ tasks.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tasks: [
+      { name: 'Fazer o curso', isDone: False}
+    ]
+  })
+}
+</script>
---
+++ data-binding/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Hello World {{ name }}</h2>
+</template>
+<script>
+  export default {
+    data: () => {
+      name: 'Eduardo'
+    }
+  }
+
+</script>
+<style>
+  
+</style>
